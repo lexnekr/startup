4fe0c248ada3be657302f9a113b894a0cee6bad6
Zadoiny Alexey <aleksey.zadoiny@vtb.com>
2015-11-20 17:00:33 +0300

    В настройки модуля добавлена лицензия.

---
+++ bitrix/modules/coffeediz.startup/lang/ru/include.php
@@ -0,0 +1,6 @@
+<?
+$MESS["CopyRightLink"] = "<p>Дизайн <a href=\"http://graygrids.com\">GrayGrids</a>, интеграция с 1С-Битрикс <a href=\"http://кофедизайн.рф\">Задойный А.В.</a></p>";
+$MESS[""] = "";
+$MESS[""] = "";
+$MESS[""] = "";
+?>
