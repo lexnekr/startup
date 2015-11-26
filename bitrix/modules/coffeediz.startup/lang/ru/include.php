ba04e84cc642f4fbc81fe7d0f781237d83aa9ff6
Zadoiny Alexey <aleksey.zadoiny@vtb.com>
2015-11-26 10:48:22 +0300

    charset bugfix

--- bitrix/modules/coffeediz.startup/lang/ru/include.php
+++ bitrix/modules/coffeediz.startup/lang/ru/include.php
@@ -1,6 +1,9 @@
 <?
 $MESS["CopyRightLink"] = "<p>Дизайн <a href=\"http://graygrids.com\">GrayGrids</a>, интеграция с 1С-Битрикс <a href=\"http://кофедизайн.рф\">Задойный А.В.</a></p>";
 $MESS["CopyRightError"] = '<script>alert("Ошибка лицензии решения STARTUP!\nПо вопросам лицензирования обратитесь в разработчику решения a@coffeediz.ru");</script>';
-$MESS[""] = "";
+$MESS["Rights"] = "Права доступа";
+$MESS["RightsSettings"] = "Настройка прав доступа";
+$MESS["Save"] = "Сохранить";
+$MESS["Cancel"] = "Отменить";
 $MESS[""] = "";
 ?>
