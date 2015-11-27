2c05d309a0c0cd3c07bab6c6100ebe3758f039ef
Zadoiny Alexey <aleksey.zadoiny@vtb.com>
2015-11-27 16:11:10 +0300

    v.1.0.1
    * Добавлена поддержка LIMITED лицензий
    * Добавлены шаблоны компонента Google.Maps идентичные системным (v15.5) с добавлением поддержки КОМПОЗИТНОГО режима в виде полностью динамической области
    * Исправлена опечатка в названии шаблона сайта

--- bitrix/modules/coffeediz.startup/install/components/coffeediz/copyright/lang/ru/component.php
+++ bitrix/modules/coffeediz.startup/install/components/coffeediz/copyright/lang/ru/component.php
@@ -1,7 +1,7 @@
 <?
 $MESS ['COPYRIGHT_name_ERROR'] = 'ОШИБКА! Отсутствует лицензионный ключ решения STURTUP!';
-$MESS ['COPYRIGHT_FREE'] = '<p>Дизайн <a href="http://graygrids.com">GrayGrids</a>, интеграция с 1С-Битрикс <a href="http://кофедизайн.рф">Задойный А.В.</a></p>';
-$MESS ['COPYRIGHT_COFFEE'] = '<p>Интеграция дизайна с 1С-Битрикс <a href="http://кофедизайн.рф">Задойный А.В.</a></p>';
+$MESS ['COPYRIGHT_FREE'] = '<p>Дизайн <a href="http://graygrids.com">GrayGrids</a>, интеграция с 1С-Битрикс <a href="http://кофедизайн.рф/solutions/php/startup/">Задойный А.В.</a></p>';
+$MESS ['COPYRIGHT_COFFEE'] = '<p>Интеграция дизайна с 1С-Битрикс <a href="http://кофедизайн.рф/solutions/php/startup/">Задойный А.В.</a></p>';
 $MESS ['COPYRIGHT_GRAYGRIDS'] = '<p>Дизайн <a href="http://graygrids.com">GrayGrids</a></p>';
 $MESS [''] = '';
 $MESS [''] = '';
